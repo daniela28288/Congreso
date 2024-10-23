609900568c92233305afeb8113933ed85634da9d
Luis Toncel <ltoncel@misena.edu.co>
2024-10-23 18:11:09 -0500

    Update 24-10-23 18:11

---
+++ index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <title>VI CONGRESO NACIONAL DE INVESTIGACIÓN E INNOVACIÓN</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="shortcut icon" href="img/logo-sin-letras.png" />
+</head>
+
+<body>
+    <div class="inicio"></div>
+    <header>
+        <img src="img/logo_fondo blanco.png" alt="">
+        <input type="checkbox" id="check">
+        <label for="check" class="abrir-menu">
+            &#8801
+        </label>
+        <div class="menu">
+            <div class="navegador">
+                <a href="#inicio">Inicio</a>
+                <a href="#registrate">Registrese</a>
+            </div>
+            <div class="redes_sociales">
+                <a href=""><img class="redes" src="img/iconos de redes-03.svg" alt=""></a>
+                <a href=""><img class="redes" src="img/iconos de redes_Mesa de trabajo 1.svg" alt=""></a>
+                <a href=""><img class="redes" src="img/iconos de redes-02.svg" alt=""></a>
+            </div>
+            <label for="check" class="cerrar-menu">
+                &#215
+            </label>
+        </div>
+    </header>
+    <div class="banner">
+        <img src="img/backgrud completo_.jpeg.jpeg" alt="" class="img_señ">
+        <div class="contenido">
+            <h1>IV Congreso Nacional de<br>INVESTIGACIÓN E INNOVACIÓN</h1>
+            <h3>Impulsando la producción para la vida<br>y la transición energética en La Guajira</h3>
+            <p class="info_con">
+                Conferencias magistrales nacionales e internacionales, <br>
+                ponencias científicas, evento certificado y <br>
+                feria de muestra comercial y empresarial del sector productivo.
+            </p>
+            <div class="fecha">
+                <img src="img/icono de fecha.svg" alt="">
+                <p>18 y 19 de Nov 2024</p>
+            </div>
+            <div class="ubi">
+                <img src="img/icono lugar.svg" alt="">
+                <p>Teatro Akuaipia - Riohacha La Guajira</p>
+            </div>
+            <img src="img/logo_sennova y sena.png" alt="" class="sena_sennova">
+        </div>
+    </div>
+    <div class="banner2">
+        <img src="img/backgrud completo_.jpeg.jpeg" alt="" class="img_g">
+        <div class="contenido2">
+            <h1>IV Congreso Nacional de<br>INVESTIGACIÓN E INNOVACIÓN</h1>
+            <h3>Impulsando la producción para la vida<br>y la transición energética en La Guajira</h3>
+            <p class="info_con">
+                Conferencias magistrales nacionales e internacionales, <br>
+                ponencias científicas, evento certificado y <br>
+                feria de muestra comercial y empresarial del sector productivo.
+            </p>
+            <div class="fecha">
+                <img src="img/icono de fecha.svg" alt="">
+                <p>18 y 19 de Nov 2024</p>
+            </div>
+            <div class="ubi">
+                <img src="img/icono lugar.svg" alt="">
+                <p>Teatro Akuaipia - Riohacha La Guajira</p>
+            </div>
+            <div class="imagen-sennova"><img src="img/logo_sennova y sena.png" alt="" class="sena_sennova"></div>
+        </div>
+    </div>
+
+    <div class="informacion">
+        <h1>¿Por qué asistir?</h1>
+        <br>
+        <p>Este es un evento arbitrado con un escenario de carácter académico, científico y empresarial, que busca
+            generar y transferir conocimiento en investigación e innovación agroempresarial entre los diferentes actores
+            y con una visión prospectiva al año 2030, aportando al desarrollo del país</p>
+    </div>
+    <div class="container-numeros">
+        <br><br><br><br><br>
+        <h1>EXPANDE TUS CONOCIMIENTOS</h1>
+        <br>
+        <div class="container-iconos-numericos">
+            <img class="icono-numero" src="img/Numerosssconferencistas copia-01.svg" alt="conferencistas">
+            <img class="icono-numero" src="img/Numerosssconferencistas copia-03.svg" alt="expositores">
+            <img class="icono-numero" src="img/Numerosssconferencistas copia-02.svg" alt="empresas">
+        </div>
+        <br><br><br><br>
+    </div>
+    <div class="container-lineas-tematicas">
+        <h1>EJES TEMÁTICOS</h1>
+        <h4>¿Hacia qué áreas de investigación específicas está encaminado?</h4>
+        <div class="tematicas">
+            <a href="primer-eje.html"><div class="tematica">
+                <img src="img/acuicultura y pesca.jpeg" alt="" class="imagen_tematica">
+                <img src="img/Eje_Tematico_Vertical azul-01.png" alt="" class="icono_tematico">
+            </div></a>
+            <a href="segundo-eje.html"><div class="tematica">
+                <img src="img/agroindustria.JPG" alt="" class="imagen_tematica">
+                <img src="img/Eje_Tematico_Vertical azul-02.png" alt="" class="icono_tematico">
+            </div></a>
+            <a href="tercer-eje.html"><div class="tematica">
+                <img src="img/agropecuario.jpg" alt="" class="imagen_tematica">
+                <img src="img/Eje_Tematico_Vertical azul-03.png" alt="" class="icono_tematico">
+            </div></a>
+            <a href="cuarto-eje.html"><div class="tematica">
+                <img src="img/transcion energetica.jpg" alt="" class="imagen_tematica">
+                <img src="img/Eje_Tematico_Vertical azul-04.png" alt="" class="icono_tematico">
+            </div></a>
+        </div>
+    </div>
+    <div class="contenedor-recuadros">
+        <h1>VENTAJAS Y BENEFICIOS</h1>
+        <p class="p_po">¿Por qué deberías unirte a esta experiencia?</p>
+        <div class="recuadros1">
+            <div class="recuadro-informacion">
+                <img class="icon" src="img/conferencistas-01.png" alt=""><br>
+                <h3>Conferencistas</h3><br>
+                <p>Inmersión de dos días con conferencistas
+                    de alto nivel, reconocidos por su experiencia y liderazgo en
+                    cada uno de los enfoques presentados.</p>
+            </div>
+            <div class="recuadro-informacion">
+                <img class="icon" src="img/Diploma-01.png" alt=""><br>
+                <h3>Certificado</h3><br>
+                <p>Descargue su certificado virtual de
+                    asistencia dle congreso IV Congreso Nacional de
+                    Investigacion e Innovacion, impulsando la producción
+                    para la vida y la transición energética en La Guajira.</p>
+
+            </div>
+            <div class="recuadro-informacion">
+                <img class="icon" src="img/tienda-01.png" alt=""><br>
+                <h3>Feria empresarial</h3><br>
+                <p>Propiciar un espacio comercial y empresarial donde empresas
+                    del sector productivo presentarán sus productos y servicios.</p>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="recuadros2">
+            <div class="recuadro-informacion">
+                <img class="icon" src="img/ideas-01.png" alt=""><br>
+                <h3>Exposición proyectos</h3><br>
+                <p>Promueve un espacio de encuentro para el intercambio de
+                    conocimientos entre los diferentes agentes del ecosistema de ciencia,
+                    tecnología e innovación del departamento de La Guajira y la región Caribe.</p>
+            </div>
+            <div class="recuadro-informacion">
+                <img class="icon" src="img/Publicacion de memorias-01.png" alt=""><br>
+                <h3>Publicación de memoria</h3><br>
+                <p>La publicación de todos los artículos presentados en ponencia oral que
+                    cuenten con autorización del autor se realizará en el libro de memorias
+                    con código ISBN.</p>
+            </div>
+            <div class="recuadro-informacion">
+                <img class="icon" src="img/premio-01.png" alt=""><br>
+                <h3>Premios</h3><br>
+                <p>Las ponencias de cada una de las temáticas tendrán una evaluación del comité
+                    científico de cada especialidad, para otorgar la distinción de “Mejor ponencia
+                    en el congreso”.</p>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br><br>
+    <div class="container-fondo-expositores">
+        <div  id="primer-expositor" class="expositor">
+            <img class="imagen-expositor" src="img/primer-expositor.png" alt="">
+            <img class="linea" src="img/linea-seccion-expositores.png" alt="">
+            <div class="textt">
+                <h1>JORGE ENRIQUE</h1>
+                <h4>PÁRAMO GRANADOS</h4>
+            </div>
+            <div class="infoperfil">
+                <img src="img/icons8-podio-con-altavoz-48.png" alt="">
+                Ingeniero Pesquero de la Universidad del Magdalena. Magíster (M.Sc.) en Pesquerías de la Universidad de Concepción, Chile. Doctorado (Ph.D.) en Ciencias Naturales, Universidad de Bremen, Alemania.
+            </div>
+        </div>
+        <div class="expositor">
+            <img class="imagen-expositor" src="img/segundo-expositor.png" alt="">
+            <img class="linea" src="img/linea-seccion-expositores.png" alt="">
+            <div class="textt">
+                <h1>JUAN CARLOS</h1>
+                <h4>VALDELAMAR VILLEGAS</h4>
+            </div>
+            <div class="infoperfil">
+                <img src="img/icons8-podio-con-altavoz-48.png" alt="">
+                Biólogo, con Especialización en Estadística Aplicada, Maestría en Ciencias Ambientales y Doctorado en Toxicología Ambiental.
+            </div>
+        </div>
+        <div  id="ultimo-expositor" class="expositor">
+            <img class="imagen-expositor" src="img/tercer-expositor.png" alt="">
+            <img class="linea" src="img/linea-seccion-expositores.png" alt="">
+            <div class="textt">
+                <h1>JUAN CARLOS</h1>
+                <h4>OSPINA CHIVIRI</h4>
+            </div>
+            <div class="infoperfil">
+                <img src="img/icons8-podio-con-altavoz-48.png" alt="">
+                Médico veterinario y zootecnista con especialidad en patología anatómica veterinaria y estudios de maestría en microbiologia.
+            </div>
+        </div>
+    </div>
+    <div id="registrate"></div>
+    <div class="container-registro">
+        <div class="contenido1">
+            <h1>TENGA EN CUENTA QUE: </h1>
+            <p>Si va a realizar su registro como <b>ponente</b> lea atentamente los documentos adjuntos antes de su
+                registro</p>
+            <div class="contenedor-register">
+                <div class="anclas-registro">
+                    <a href="./docs/Formato-Plantilla-Presentacion-PowerPoint.pptx" target="_blank">
+                        <div class="ancla">
+                            <img src="img/icono_De_ANcla-02.png" alt="">
+                            <p>Formato plantilla presentación PowerPoint</p>
+                        </div>
+                    </a>
+                    <a href="./docs/Formato-Carta-de-Originalidad.doc" target="_blank">
+                        <div class="ancla">
+                            <img src="img/icono_De_ANcla-03.png" alt="">
+                            <p>Formato carta de originalidad</p>
+
+                        </div>
+                    </a>
+                    <a href="./docs/Guia-para-presentar-su-trabajo-de-investigacion-IV-congreso.pdf" target="_blank">
+                        <div class="ancla">
+                            <img src="img/icono_De_ANcla-01.png" alt="">
+                            <p>Guía para presentar su trabajo de investigación IV congreso</p>
+
+                        </div>
+                    </a>
+                    <a href="./docs/Terminos-de-Referencia-IV-Congreso.pdf" target="_blank">
+                        <div class="ancla">
+                            <img id="seccion-registrate" src="img/icono_De_ANcla-01.png" alt="">
+                            <p>Términos de referencia 2024 IV CONGRESO</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="contenido2">
+            <h1>Regístrate</h1>
+            <a href="https://congresoinsenacaa.com/asistente" class="todo" target="_blank">
+                <div class="opciones">
+                    <h6>Asistente</h6>
+                </div>
+                <div class="flecha">
+                    <img src="img/arrow-right-solid (1).svg" alt="">
+                </div>
+            </a>
+            <!-- <a href="https://congresoinsenacaa.com/evaluador" class="todo" target="_blank">
+                <div class="opciones">
+                <h6>Evaluador</h6>
+            </div>
+            <div class="flecha">
+                <img src="img/arrow-right-solid (1).svg" alt="">
+            </div>
+            </a> -->
+            <a href="https://congresoinsenacaa.com/ponente" class="todo" target="_blank">
+                <div class="opciones">
+                    <h6>Ponente</h6>
+                </div>
+                <div class="flecha">
+                    <img src="img/arrow-right-solid (1).svg" alt="">
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="container-ultimo-banner">
+        <h1>Teatro Akuaipia</h1>
+        <p>Centro de Desarrollo Educativo Comfaguajira</p>
+        <h3>Riohacha, La Guajira</h3>
+    </div>
+    <div class="container-patrocinadores">
+        <div class="apoya">
+            <h3 id="texto-apoya">Apoya</h3>
+            <img id="logo-inca" src="img/log inca-01.png" alt="">
+        </div>
+        <div class="organiza">
+            <h3>Organiza</h3>
+            <img src="img/logo_sennova y sena.png" alt="">
+        </div>
+    </div>
+    <footer>
+        <div class="footer-container">
+            <div class="left-side">
+                <img src="img/nombre del centro_Mesa de trabajo 1.png" alt="Centro">
+            </div>
+
+            <!-- Lado derecho: Información -->
+            <div class="info">
+                <div class="icono-informacion">
+                    <img src="img/icono lugarBlanco.svg" alt="Ubicación">
+                    <p>Kilómetro 1 Salida a Barrancas</p>
+
+                </div>
+                <p>Fonseca La Guajira</p>
+                <p>PBX 57 601 5461500</p>
+
+                <div class="correo">
+                    <img src="img/iconoCorreo_Mesa de trabajo 1.png" alt="Correo">
+                    <p>info@congresoinsenacaa.com</p>
+                </div>
+                <div class="correo">
+                    <img src="img/iconoCorreo_Mesa de trabajo 1.png" alt="Correo">
+                    <p>egarrido@sena.edu.co</p>
+                </div>
+                <div class="correo">
+                    <img src="img/iconoCorreo_Mesa de trabajo 1.png" alt="Correo">
+                    <p>bwild@sena.edu.co</p>
+                </div>
+                <div class="icon-whatsapp">
+                    <img src="img/whatsapp-01.svg" alt="Whatsapp">
+                    <p>301 221 9139</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Copyright -->
+    <p class="p">Copyright &copy;
+        <?php echo date("Y"); ?>
+        Todos los derechos reservados. By ADSO !Ng LUIS T😎NCEL. Leo Carrillo.
+    </p>
+</body>
+
+<script>
+    // document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    //     anchor.addEventListener("click", function (e) {
+    //         e.preventDefault();
+
+    //         document.querySelector(this.getAttribute("href")).scrollIntoView({
+    //             behavior: "smooth",
+    //         });
+    //     });
+    // });
+    function smoothScroll(target, duration) {
+        let targetElement = document.querySelector(target);
+        let targetPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+        let startPosition = window.scrollY;
+        let distance = targetPosition - startPosition;
+        let startTime = null;
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            let timeElapsed = currentTime - startTime;
+            let run = ease(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function ease(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+
+        requestAnimationFrame(animation);
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            smoothScroll(this.getAttribute('href'), 2500); // 1500ms = 1.5 segundos
+        });
+    });
+
+</script>
+
+</html>
