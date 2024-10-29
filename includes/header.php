fa756dd6e90d58a79a8a64e9dd647e9840936169
Luis Toncel <ltoncel@misena.edu.co>
2024-10-29 16:31:05 -0500

    Se actualizo el header

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,22 @@
 <div class="inicio"></div>
-    <header>
-        <img class="logo" src="img/logo.png" alt="">
-        <input type="checkbox" id="check">
-        <label for="check" class="abrir-menu">
-            &#8801
-        </label>
-        <div class="menu">
-            <div class="navegador">
-                <a href="index.php">Inicio</a>
-                <a href="index.php#registrate">Registrese</a>
-            </div>
-            <div class="redes_sociales">
-                <a href=""><img class="redes" src="img/iconos de redes-03.svg" alt=""></a>
-                <a href=""><img class="redes" src="img/iconos de redes_Mesa de trabajo 1.svg" alt=""></a>
-                <a href=""><img class="redes" src="img/iconos de redes-02.svg" alt=""></a>
-            </div>
-            <label for="check" class="cerrar-menu">
-                &#215
-            </label>
+<header>
+    <img class="logo" src="img/logo.png" alt="">
+    <input type="checkbox" id="check">
+    <label for="check" class="abrir-menu">
+        &#8801
+    </label>
+    <div class="menu">
+        <div class="navegador">
+            <a href="index.php">Inicio</a>
+            <a href="index.php#registrate">Registrese</a>
         </div>
-    </header>
+        <div class="redes_sociales">
+            <a href=""><img class="redes" src="img/iconos de redes-03.svg" alt=""></a>
+            <a href=""><img class="redes" src="img/iconos de redes_Mesa de trabajo 1.svg" alt=""></a>
+            <a href=""><img class="redes" src="img/iconos de redes-02.svg" alt=""></a>
+        </div>
+        <label for="check" class="cerrar-menu">
+            &#215
+        </label>
+    </div>
+</header>
